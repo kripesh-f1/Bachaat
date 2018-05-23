ee892cbcffa62f7e6f313bb3480f2d578cbefb4a
suminshakyaf1soft <sumin.shakya@f1soft.com>
2018-05-23 11:53:40 +0545

    Insert query for user

--- src/main/resources/db-install/data.sql
+++ src/main/resources/db-install/data.sql
@@ -3,9 +3,13 @@
 INSERT INTO tbl_user(first_name,middle_name,last_name,password,mobile_number,email_address,address,activation_code)
 VALUES ('admin','admin','admin','admin','1234567890','admin@admin.com','admin',12345);
 
-/* TO INSERT RECORD IN TBL_AUTHORITY */
+/* TO INSERT RECORD OF ADMIN IN TBL_AUTHORITY  */
 INSERT INTO tbl_authority(authority_name)
 VALUES ('admin');
+
+/* TO INSERT RECORD OF USER IN TBL_AUTHORITY */
+INSERT INTO tbl_authority(authority_name)
+VALUES ('user');
 
 /* TO INSERT RECORD IN USER_AUTHORITY */
 INSERT INTO user_authority(user_id,authority_id)
