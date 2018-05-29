d02fe7674944c22c52c669c4dcea6caee169ea1c
kripesh-f1 <kripesh.bista@f1soft.com>
2018-05-29 12:57:42 +0545

    Remove message variable from DataNotFoundException

--- src/main/java/com/f1soft/bachaat/exception/DataNotFoundException.java
+++ src/main/java/com/f1soft/bachaat/exception/DataNotFoundException.java
@@ -2,15 +2,7 @@ package com.f1soft.bachaat.exception;
 
 public class DataNotFoundException extends RuntimeException
 {
-    private String message;
-
     public DataNotFoundException(String message){
         super(message);
-    }
-
-    @Override
-    public String getMessage()
-    {
-        return message;
     }
 }
