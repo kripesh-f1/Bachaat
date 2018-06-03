2ce4f9affe8760e71c504f45ea7fa5a527bf0784
nitishresthaf1soft <nitish.shrestha@f1soft.com>
2018-06-03 15:58:07 +0545

    Correct message in logger

--- src/test/java/com/f1soft/bachaat/service/TestMenuService.java
+++ src/test/java/com/f1soft/bachaat/service/TestMenuService.java
@@ -51,7 +51,7 @@ public class TestMenuService {
 
     @Test(expected = MenuAlreadyExistsException.class)
     public void Should_ThrowException_WhenMenuDataIsDuplicate() {
-        logger.info("Inside Test Menu Add when same menu data is passed");
+        logger.info("Inside Test Menu Add when same menu data is duplicate");
         when(menuRepository.findByNameAndLinkAndParentId(menu.getName(), menu.getLink(), menu.getParentId())).thenThrow(MenuAlreadyExistsException.class);
         menuService.addMenu(menu);
     }
